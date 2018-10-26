e8244d28e4af33682151198c4b059d5c976779d0
Isaac Elliott <isaace71295@gmail.com>
2018-10-26 13:43:09 +1000

    It's 'working'

--- src/Monoidal.hs
+++ src/Monoidal.hs
@@ -1,3 +1,4 @@
+{-# language FlexibleContexts #-}
 {-# language FlexibleInstances #-}
 {-# language DataKinds #-}
 {-# language ConstraintKinds, GADTs #-}
@@ -10,12 +11,14 @@
 {-# language ScopedTypeVariables #-}
 {-# language TypeApplications #-}
 
-{-# LANGUAGE TemplateHaskell #-}
+{-# language TemplateHaskell #-}
 {-# OPTIONS_GHC -O -fplugin Test.Inspection.Plugin #-}
 
 module Monoidal where
 
 import Prelude hiding (id, (.), Functor(..), const, uncurry, fst, snd, curry)
+import Unsafe.Coerce
+import Data.Coerce
 import Data.Kind
 import Data.Void
 import Test.Inspection
@@ -28,7 +31,9 @@ class Category (arr :: k -> k -> Type) where
   id :: a `arr` a
   (.) :: b `arr` c -> a `arr` b -> a `arr` c
 
-class Category (arr :: k -> k -> Type) => Product arr (p :: k -> k -> k) | p -> arr where
+class Category (arr :: k -> k -> Type) => Product arr (p :: k -> k -> k)
+  | p -> arr, arr -> p -- products are unique up to isomorphism, so there only needs to be one instance per category
+  where
   fst :: p a b `arr` a
   snd :: p a b `arr` b
   intro
@@ -36,19 +41,37 @@ class Category (arr :: k -> k -> Type) => Product arr (p :: k -> k -> k) | p -> 
     -> y `arr` b
     -> y `arr` p a b
 
+instance Product (->) (,) where
+  {-# inline fst #-}
+  fst (a, _) = a
+
+  {-# inline snd #-}
+  snd (_, b) = b
+
+  {-# inline intro #-}
+  intro f g x = (f x, g x)
+
 prodAssoc :: Product arr p => p a (p b c) `arr` p (p a b) c
 prodAssoc = intro (intro fst (fst . snd)) (snd . snd)
+
+prodDeassoc :: Product arr p => p (p a b) c `arr` p a (p b c)
+prodDeassoc = intro (fst . fst) (intro (snd . fst) snd)
 
 prodSwap :: Product arr p => p a b `arr` p b a
 prodSwap = intro snd fst
 
-class Category (arr :: k -> k -> Type) => Coproduct arr (p :: k -> k -> k) | p -> arr where
+class Category (arr :: k -> k -> Type) => Coproduct arr (p :: k -> k -> k) | p -> arr, arr -> p where
   inl :: a `arr` p a b
   inr :: b `arr` p a b
   elim
     :: a `arr` y
     -> b `arr` y
     -> p a b `arr` y
+
+instance Coproduct (->) Either where
+  inl = Left
+  inr = Right
+  elim = either
 
 class Product (arr :: k -> k -> Type) p => Exponential arr p (e :: k -> k -> k)
   | e -> arr p, arr -> p e -- not sure about this one
@@ -63,11 +86,23 @@ class Category arr => Terminating (arr :: k -> k -> Type) where
   type Terminal arr :: k
   terminal :: a `arr` Terminal arr
 
+instance Terminating (->) where
+  type Terminal (->) = ()
+
+  {-# inline terminal #-}
+  terminal _ = ()
+
 powerOfOne_to :: (Terminating arr, Exponential arr p e) => e (Terminal arr) a `arr` Terminal arr
 powerOfOne_to = terminal
 
 powerOfOne_from :: (Terminating arr, Exponential arr p e) => Terminal arr `arr` e (Terminal arr) a
 powerOfOne_from = curry fst
+
+expPoints_to :: (Terminating arr, Exponential arr p e) => Terminal arr `arr` e y x -> x `arr` y
+expPoints_to f = uncurry f . intro terminal id
+
+expPoints_from :: (Terminating arr, Exponential arr p e) => x `arr` y -> Terminal arr `arr` e y x
+expPoints_from f = curry (f . snd)
 
 firstPower_to :: forall a arr p e. (Terminating arr, Exponential arr p e) => e a (Terminal arr) `arr` a
 firstPower_to = apply . intro id terminal
@@ -81,8 +116,14 @@ expProd_to =
   apply . intro @arr @p (apply . fst) snd .
   prodAssoc . intro fst (prodSwap . snd)
 
-expProd_from :: forall arr p e z y x. Exponential arr p e => e z (p y x) `arr` e (e z y) x
-expProd_from = _
+expProd_from :: Exponential arr p e => e z (p x y) `arr` e (e z y) x
+expProd_from = curry (curry (apply . prodDeassoc))
+
+expFlipConst :: Exponential arr p e => b `arr` e a a
+expFlipConst = curry snd
+
+expConst :: Exponential arr p e => a `arr` e a b
+expConst = curry fst
 
 class Category arr => Initiating (arr :: k -> k -> Type) where
   type Initial arr :: k
@@ -95,6 +136,21 @@ class (Category p, Category q, Category r) => Bifunctor p q r (f :: i -> j -> k)
     :: (a `p` b)
     -> (c `q` d)
     -> (f a c `r` f b d)
+
+-- |
+-- Take an (end) functor  f : C * C -> C
+-- if C has products, then C * C is a subcategory of of C (?)
+--
+-- so instead consider the functor   f : C -> C
+-- this functor is strong w.r.t a tensor   t : (C * C) -> C if
+-- there's a natural transformation from   a * f(b) -> f(a * b)
+class (Product k p, Tensor k p, Bifunctor k k k f) => StrongBifunctor k p f where
+  lstrength :: p a (f b c) `k` f (p a b) c
+  rstrength :: p a (f b c) `k` f b (p a c)
+
+instance StrongBifunctor (->) (,) (,) where
+  lstrength = deassoc
+  rstrength = intro (fst . snd) (intro fst (snd . snd))
 
 first :: Bifunctor p q r f => p a b -> f a c `r` f b c
 first f = bimap f id
@@ -116,9 +172,6 @@ class Bifunctor arr arr arr t => Tensor arr (t :: k -> k -> k) where
 
   unitRightTo :: t a (Unit t) `arr` a
   unitRightFrom :: a `arr` t a (Unit t)
-
-class Tensor arr t => Symmetric arr (t :: k -> k -> k) where
-  swap :: t a b `arr` t b a
 
 -- left :: Bifunctor p q r t => p a b -> t a c `arr` t b c
 -- left = _
@@ -208,14 +261,21 @@ instance Bifunctor (->) (->) (->) (,) where
 
 instance Tensor (->) (,) where
   type Unit (,) = ()
+
+  {-# inline assoc #-}
   assoc ((a, b), c) = (a, (b, c))
 
+  {-# inline deassoc #-}
   deassoc (a, (b, c)) = ((a, b), c)
 
+  {-# inline unitLeftTo #-}
   unitLeftTo ((), a) = a
+  {-# inline unitLeftFrom #-}
   unitLeftFrom = (,) ()
 
+  {-# inline unitRightTo #-}
   unitRightTo (a, ()) = a
+  {-# inline unitRightFrom #-}
   unitRightFrom a = (a, ())
 
 
@@ -224,6 +284,11 @@ instance Tensor (->) (,) where
 newtype Optic k p s t a b
   = Optic
   { unOptic
+    -- forall x. Optic (s -> (,)    a x) ((,)       b x -> t)
+    -- forall x. Optic (s -> Either a x) (Either    b x -> t)
+    --
+    -- forall x. Optic (s `k` tensor a x) (tensor b x `k` t)
+    --
     -- We can't do 'forall x. TupleK (s -> p a x) (p b x -> t)' or something
     -- like it. This way we get true existential quantification on x
     :: forall r. (forall x. (s `k` p a x) -> (p b x `k` t) -> r) -> r
@@ -245,7 +310,6 @@ composeOptic (Optic o1) (Optic o2) =
   o2 $ \sx yt ->
   Optic $ \f -> f (assoc . first xa . sx) (yt . first by . deassoc)
 
-
 {-# inline o #-}
 o
   :: Tensor k p
@@ -254,85 +318,94 @@ o
   -> Optic k p s t a b
 o = flip composeOptic
 
-set :: forall k p prod e s t a b. (Symmetric k p, Exponential k prod e) => Optic k p s t a b -> prod b s `k` t
--- set (Optic o1) b = o1 $ \f g -> g . first (const b) . f
-set (Optic o1) =
-  o1 (\f g -> _ . intro @k @prod fst (f . snd))
-
-{-
+set
+  :: forall k p prod s t a b
+   . ( StrongBifunctor k prod p
+     , Tensor k p
+     , Product k prod
+     )
+  => Optic k p s t a b
+  -> prod b s `k` t
+set (Optic o1) = o1 (\f g -> g . first fst . lstrength . intro @k @prod fst (f . snd))
 
 ---- Lenses ----
 
-type Lens = Optic TupleK
+get :: (Tensor k p, Product k p) => Optic k p s t a b -> s `k` a
+get (Optic o1) = o1 $ \f _ -> fst . f
 
-get :: Lens s t a b -> s -> a
-get (Optic o1) s = o1 $ \f _ -> fstK (f s)
-
-lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens :: (Tensor k p, Product k p) => (s `k` a) -> (p s b `k` t) -> Optic k p s t a b
 lens sa sbt =
-  Optic $ \f -> f (\s -> tuple (sa s) s) (\a -> unTupleK a $ flip sbt)
+  -- Optic $ \f -> f (\s -> tuple (sa s) s) (\a -> unTupleK a $ flip sbt)
+  Optic $ \f -> f (intro sa id) (sbt . prodSwap)
 
-_1 :: Lens (a, c) (b, c) a b
-_1 = lens fst (\(_, b) a -> (a, b))
+_1 :: (Tensor k p, Product k p) => Optic k p (p a c) (p b c) a b
+_1 = lens fst (intro snd (snd . fst))
 
-_2 :: Lens (c, a) (c, b) a b
-_2 = lens snd (\(a, _) b -> (a, b))
+_2 :: (Tensor k p, Product k p) => Optic k p (p c a) (p c b) a b
+_2 = lens snd (intro (fst . fst) snd)
 
 
 ---- Prisms ----
 
-type Prism = Optic EitherK
+{-# inline preview #-}
+preview :: (Tensor k p, Coproduct k p, Terminating k) => Optic k p s t a b -> s `k` p (Terminal k) a
+preview (Optic o1) = o1 $ \f _ -> elim inr (inl . terminal) . f
 
-preview :: Prism s t a b -> s -> Maybe a
-preview (Optic o1) = o1 $ \f _ -> (\a -> unEitherK a Just (const Nothing)) . f
+review :: (Tensor k p, Coproduct k p) => Optic k p s t a b -> b `k` t
+review (Optic o1) = o1 $ \_ g -> g . inl
 
-review :: Prism s t a b -> b -> t
-review (Optic o1) = o1 $ \_ g -> g . left
+{-# inline prism #-}
+prism :: (Tensor k p, Coproduct k p) => (s `k` p a t) -> (b `k` t) -> Optic k p s t a b
+prism pre re = Optic $ \f -> f (elim inl inr . pre) (elim re id)
 
-prism :: (s -> Either a t) -> (b -> t) -> Prism s t a b
-prism pre re = Optic $ \f -> f (either left right . pre) (\a -> unEitherK a re id)
+{-# inline _Left #-}
+_Left :: (Tensor k p, Coproduct k p) => Optic k p (p a c) (p b c) a b
+_Left = prism (elim inl (inr . inr)) inl
 
-_Left :: Prism (Either a c) (Either b c) a b
-_Left = prism (either Left (Right . Right)) Left
+{-# inline _Right #-}
+_Right :: (Tensor k p, Coproduct k p) => Optic k p (p c a) (p c b) a b
+_Right = prism (elim (inr . inl) inl) inr
 
-_Right :: Prism (Either c a) (Either c b) a b
-_Right = prism (either (Right . Left) Left) Right
+{-# inline _Just #-}
+_Just :: (Tensor (->) p, Coproduct (->) p) => Optic (->) p (Maybe a) (Maybe b) a b
+_Just = prism (maybe (inr Nothing) inl) Just
 
-_Just :: Prism (Maybe a) (Maybe b) a b
-_Just = prism (maybe (Right Nothing) Left) Just
+_True :: (Tensor (->) p, Coproduct (->) p) => Optic (->) p Bool Bool () ()
+_True = prism (\b -> if b then inl () else inr b) (\() -> True)
 
-_True :: Prism Bool Bool () ()
-_True = prism (\b -> if b then Left () else Right b) (\() -> True)
-
-_Nothing :: Prism (Maybe a) (Maybe a) () ()
-_Nothing = prism (maybe (Left ()) (Right . Just)) (\() -> Nothing)
+_Nothing :: (Tensor (->) p, Coproduct (->) p) => Optic (->) p (Maybe a) (Maybe a) () ()
+_Nothing = prism (maybe (inl ()) (inr . Just)) (\() -> Nothing)
 
 
 ---- Inspection Tests for fusion ----
 
+get_222, get_222' :: (a, (b, (c, d))) -> d
 get_222 = get (_2 `o` (_2 `o` _2))
 get_222' (_, (_, (_, a))) = a
 
 inspect ('get_222 === 'get_222')
 
-
+set_222, set_222' :: (e, (a, (b, (c, d)))) -> (a, (b, (c, e)))
 set_222 = set (_2 `o` (_2 `o` _2))
 -- which is preferable?
-set_222' d (a, x) = (a, case x of; (b, x') -> (b, case x' of; (c, _) -> (c, d)))
-set_222'' d (a, (b, (c, _))) = (a, (b, (c, d)))
+set_222' (d, (a, x)) = (a, case x of; (b, x') -> (b, case x' of; (c, _) -> (c, d)))
+set_222'' (d, (a, (b, (c, _)))) = (a, (b, (c, d)))
 
-inspect ('set_222 === 'set_222')
+-- inspect ('set_222 === 'set_222')
+inspect ('set_222 === 'set_222'')
 
 
+preview_LJR, preview_LJR' :: Either (Maybe (Either a b)) c -> Either () b
 preview_LJR = preview (_Left `o `(_Just `o` _Right))
 preview_LJR' x =
   case x of
-    Left (Just (Right a)) -> Just a
-    _ -> Nothing
+    Left (Just (Right a)) -> Right a
+    _ -> Left ()
 
 inspect ('preview_LJR === 'preview_LJR')
 
 
+{-
 review_LJR = review (_Left `o `(_Just `o` _Right))
 review_LJR' = Left . Just . Right
 
