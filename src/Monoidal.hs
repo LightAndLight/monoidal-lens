8ad08cfe1ac39e147d9c81c8036d2d4cef889320
Isaac Elliott <isaace71295@gmail.com>
2018-10-27 12:29:55 +1000

    More stuff

--- src/Monoidal.hs
+++ src/Monoidal.hs
@@ -1,8 +1,10 @@
+{-# language EmptyCase #-}
 {-# language FlexibleContexts #-}
 {-# language FlexibleInstances #-}
 {-# language DataKinds #-}
 {-# language ConstraintKinds, GADTs #-}
 {-# language MultiParamTypeClasses, FunctionalDependencies #-}
+{-# language TypeFamilyDependencies #-}
 {-# language TypeFamilies #-}
 {-# language TypeOperators #-}
 {-# language RankNTypes #-}
@@ -12,28 +14,41 @@
 {-# language TypeApplications #-}
 
 {-# language TemplateHaskell #-}
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 {-# OPTIONS_GHC -O -fplugin Test.Inspection.Plugin #-}
 
 module Monoidal where
 
 import Prelude hiding (id, (.), Functor(..), const, uncurry, fst, snd, curry)
-import Unsafe.Coerce
+import qualified Prelude
 import Data.Coerce
+import Data.Functor.Identity
 import Data.Kind
+import Data.Proxy
 import Data.Void
 import Test.Inspection
 
+import qualified Data.Functor.Day as Functor
+import qualified Data.Functor.Sum as Functor
+import qualified Data.Functor.Product as Functor
+
+class Vacuous (a :: *) where
+instance Vacuous a where
+
 instance Category (->) where
+  type Obj (->) = Vacuous
   id = \a -> a
   (.) = \f g x -> f (g x)
 
 class Category (arr :: k -> k -> Type) where
+  type Obj arr :: k -> Constraint
   id :: a `arr` a
   (.) :: b `arr` c -> a `arr` b -> a `arr` c
 
 class Category (arr :: k -> k -> Type) => Product arr (p :: k -> k -> k)
   | p -> arr, arr -> p -- products are unique up to isomorphism, so there only needs to be one instance per category
   where
+
   fst :: p a b `arr` a
   snd :: p a b `arr` b
   intro
@@ -86,6 +101,12 @@ class Category arr => Terminating (arr :: k -> k -> Type) where
   type Terminal arr :: k
   terminal :: a `arr` Terminal arr
 
+class Terminating arr => Pointed arr where
+  point :: a -> Terminal arr `arr` a
+
+const :: Pointed arr => a -> b `arr` a
+const a = point a . terminal
+
 instance Terminating (->) where
   type Terminal (->) = ()
 
@@ -129,6 +150,14 @@ class Category arr => Initiating (arr :: k -> k -> Type) where
   type Initial arr :: k
   initial :: Initial arr `arr` a
 
+newtype Arr1 (a :: * -> *) (b :: * -> *)
+  = Arr1 { apArr1 :: forall x. a x -> b x }
+
+instance Category Arr1 where
+  type Obj Arr1 = Prelude.Functor
+  id = Arr1 id
+  Arr1 f . Arr1 g = Arr1 (f . g)
+
 class (Category p, Category q, Category r) => Bifunctor p q r (f :: i -> j -> k)
   | f -> p q r
   where
@@ -136,6 +165,123 @@ class (Category p, Category q, Category r) => Bifunctor p q r (f :: i -> j -> k)
     :: (a `p` b)
     -> (c `q` d)
     -> (f a c `r` f b d)
+
+instance Bifunctor Arr1 Arr1 Arr1 Functor.Day where
+  {-# inline bimap #-}
+  bimap f g = Arr1 $ \(Functor.Day x y z) -> Functor.Day (apArr1 f x) (apArr1 g y) z
+
+instance Bifunctor Arr1 Arr1 Arr1 Functor.Product where
+  {-# inline bimap #-}
+  bimap f g = Arr1 $ \(Functor.Pair x y) -> Functor.Pair (apArr1 f x) (apArr1 g y)
+
+instance Product Arr1 Functor.Product where
+  {-# inline fst #-}
+  fst = Arr1 $ \(Functor.Pair a _) -> a
+
+  {-# inline snd #-}
+  snd = Arr1 $ \(Functor.Pair _ a) -> a
+
+  {-# inline intro #-}
+  intro f g = Arr1 $ \a -> Functor.Pair (apArr1 f a) (apArr1 g a)
+
+instance Bifunctor Arr1 Arr1 Arr1 Functor.Sum where
+  {-# inline bimap #-}
+  bimap f g = Arr1 $ \a -> case a of
+    Functor.InL x -> Functor.InL (apArr1 f x)
+    Functor.InR x -> Functor.InR (apArr1 g x)
+
+instance Coproduct Arr1 Functor.Sum where
+  {-# inline inl #-}
+  inl = Arr1 Functor.InL
+  {-# inline inr #-}
+  inr = Arr1 Functor.InR
+  {-# inline elim #-}
+  elim f g =
+    Arr1 $ \a -> case a of
+      Functor.InL x -> apArr1 f x
+      Functor.InR x -> apArr1 g x
+
+instance StrongBifunctor Arr1 Functor.Product Functor.Sum where
+  {-# inline lstrength #-}
+  lstrength =
+    Arr1 $ \(Functor.Pair a b) -> case b of
+      Functor.InL c -> Functor.InL (Functor.Pair a c)
+      Functor.InR c -> Functor.InR c
+
+  {-# inline rstrength #-}
+  rstrength =
+    Arr1 $ \(Functor.Pair a b) -> case b of
+      Functor.InL c -> Functor.InL c
+      Functor.InR c -> Functor.InR (Functor.Pair a c)
+
+class (Category arr, Bifunctor arr arr arr t) => Tensor arr (t :: k -> k -> k) where
+  type Unit t :: k
+
+  assoc :: t (t a b) c `arr` t a (t b c)
+  deassoc :: t a (t b c) `arr` t (t a b) c
+
+  unitLeftTo :: Obj arr a => t (Unit t) a `arr` a
+  unitLeftFrom :: a `arr` t (Unit t) a
+
+  unitRightTo :: Obj arr a => t a (Unit t) `arr` a
+  unitRightFrom :: a `arr` t a (Unit t)
+
+class Tensor arr t => Symmetric arr t where
+  swap :: t a b `arr` t b a
+
+instance Tensor Arr1 Functor.Day where
+  type Unit Functor.Day = Identity
+  assoc = Arr1 Functor.disassoc
+  deassoc = Arr1 Functor.assoc
+
+  unitLeftTo = Arr1 Functor.elim1
+  unitLeftFrom = Arr1 Functor.intro1
+
+  unitRightTo = Arr1 Functor.elim2
+  unitRightFrom = Arr1 Functor.intro2
+
+instance Tensor Arr1 Functor.Product where
+  type Unit Functor.Product = Proxy
+  assoc = Arr1 $ \(Functor.Pair (Functor.Pair a b) c) -> Functor.Pair a (Functor.Pair b c)
+  deassoc = Arr1 $ \(Functor.Pair a (Functor.Pair b c)) -> Functor.Pair (Functor.Pair a b) c
+
+  unitLeftTo = Arr1 $ \(Functor.Pair _ a) -> a
+  unitLeftFrom = Arr1 $ \a -> Functor.Pair Proxy a
+
+  unitRightTo = Arr1 $ \(Functor.Pair a _) -> a
+  unitRightFrom = Arr1 $ \a -> Functor.Pair a Proxy
+
+data Void1 a
+
+absurd1 :: Void1 x -> a
+absurd1 a = case a of
+
+instance Prelude.Functor Void1 where; fmap _ = absurd1
+
+instance Tensor Arr1 Functor.Sum where
+  type Unit Functor.Sum = Void1
+  assoc =
+    Arr1 $ \a -> case a of
+      Functor.InL (Functor.InL a) -> Functor.InL a
+      Functor.InL (Functor.InR a) -> Functor.InR (Functor.InL a)
+      Functor.InR a -> Functor.InR (Functor.InR a)
+  deassoc =
+    Arr1 $ \a -> case a of
+      Functor.InL a -> Functor.InL (Functor.InL a)
+      Functor.InR (Functor.InL a) -> Functor.InL (Functor.InR a)
+      Functor.InR (Functor.InR a) -> Functor.InR a
+
+  unitLeftTo =
+    Arr1 $ \a -> case a of
+    Functor.InL a -> absurd1 a
+    Functor.InR a -> a
+  unitLeftFrom = Arr1 $ Functor.InR
+
+  unitRightTo =
+    Arr1 $ \a -> case a of
+      Functor.InL a -> a
+      Functor.InR a -> absurd1 a
+  unitRightFrom = Arr1 $ Functor.InL
 
 class
   ( Category (p :: i -> i -> Type)
@@ -155,24 +301,43 @@ data ProdCat
 
   ProdCat (Fst a `p` Fst b) (Snd a `q` Snd b)
 
+class (Category p, Category q) => ProdObj p q (a :: (i, j)) | p q -> a where
+instance (Category p, Category q) => ProdObj p q '(p, q) where
+
 instance (Category p, Category q) => Category (ProdCat p q) where
+  type Obj (ProdCat p q) = ProdObj p q
   id = ProdCat id id
   ProdCat f f' . ProdCat g g' = ProdCat (f . g) (f' . g')
 
 -- |
--- Take a bifunctor  f : C * C -> C
--- if C has products, then C * C is a subcategory of of C (?)
---
--- so instead consider the functor   f : C -> C
--- this functor is strong w.r.t a tensor   t : (C * C) -> C if
--- there's a natural transformation from   t(a * f(b)) -> f(t(a * b))
-class (Tensor k p, Bifunctor k k k f) => StrongBifunctor k p f where
-  lstrength :: p a (f b c) `k` f (p a b) c
-  rstrength :: p a (f b c) `k` f b (p a c)
+-- This is kinda dubious, but it gets me where I need to go
+class (Tensor k t, Bifunctor k k k f) => StrongBifunctor k t f where
+  lstrength :: t a (f b c) `k` f (t a b) c
+  rstrength :: t a (f b c) `k` f b (t a c)
 
 instance StrongBifunctor (->) (,) (,) where
-  lstrength = deassoc
-  rstrength = intro (fst . snd) (intro fst (snd . snd))
+  {-# inline lstrength #-}
+  lstrength (a, b) =
+    case b of
+      (c, d) -> ((a, c), d)
+
+  {-# inline rstrength  #-}
+  rstrength (a, b) =
+    case b of
+      (c, d) -> (c, (a, d))
+
+instance StrongBifunctor (->) (,) Either where
+  {-# inline lstrength #-}
+  lstrength (a, b) =
+    case b of
+      Left c -> Left (a, c)
+      Right c -> Right c
+
+  {-# inline rstrength  #-}
+  rstrength (a, b) =
+    case b of
+      Left c -> Left c
+      Right c -> Right (a, c)
 
 first :: Bifunctor p q r f => p a b -> f a c `r` f b c
 first f = bimap f id
@@ -184,17 +349,6 @@ instance Bifunctor (->) (->) (->) Either where
   bimap f _ (Left a) = Left (f a)
   bimap _ g (Right a) = Right (g a)
 
-class (Category arr, Bifunctor arr arr arr t) => Tensor arr (t :: k -> k -> k) where
-  type Unit t :: k
-  assoc :: t (t a b) c `arr` t a (t b c)
-  deassoc :: t a (t b c) `arr` t (t a b) c
-
-  unitLeftTo :: t (Unit t) a `arr` a
-  unitLeftFrom :: a `arr` t (Unit t) a
-
-  unitRightTo :: t a (Unit t) `arr` a
-  unitRightFrom :: a `arr` t a (Unit t)
-
 instance Tensor (->) Either where
   type Unit Either = Void
   assoc (Left (Left a)) = Left a
@@ -205,10 +359,12 @@ instance Tensor (->) Either where
   deassoc (Right (Left a)) = Left (Right a)
   deassoc (Right (Right a)) = Right a
 
+  unitLeftTo (Left a) = absurd a
   unitLeftTo (Right a) = a
   unitLeftFrom = Right
 
   unitRightTo (Left a) = a
+  unitRightTo (Right a) = absurd a
   unitRightFrom = Left
 
 newtype EitherK a b = EitherK { unEitherK :: forall r. (a -> r) -> (b -> r) -> r }
@@ -313,9 +469,8 @@ newtype Optic k p s t a b
     :: forall r. (forall x. (s `k` p a x) -> (p b x `k` t) -> r) -> r
   }
 
-
 {-# inline idOptic #-}
-idOptic :: Tensor k p => Optic k p a a a a
+idOptic :: (Obj k a, Tensor k p) => Optic k p a a a a
 idOptic = Optic $ \f -> f unitRightFrom unitRightTo
 
 {-# inline composeOptic #-}
@@ -337,6 +492,10 @@ o
   -> Optic k p s t a b
 o = flip composeOptic
 
+
+over :: (Obj k a, Obj k b, Tensor k p) => Optic k p s t a b -> (a `k` b) -> s `k` t
+over (Optic o) m = o (\f g -> g . bimap m id . f)
+
 set
   :: forall k p prod s t a b
    . ( StrongBifunctor k prod p
@@ -345,7 +504,8 @@ set
      )
   => Optic k p s t a b
   -> prod b s `k` t
-set (Optic o1) = o1 (\f g -> g . first fst . lstrength . intro @k @prod fst (f . snd))
+set (Optic o1) =
+  o1 (\f g -> g . first fst . lstrength . intro @k @prod fst (f . snd))
 
 ---- Lenses ----
 
@@ -374,7 +534,7 @@ review :: (Tensor k p, Coproduct k p) => Optic k p s t a b -> b `k` t
 review (Optic o1) = o1 $ \_ g -> g . inl
 
 {-# inline prism #-}
-prism :: (Tensor k p, Coproduct k p) => (s `k` p a t) -> (b `k` t) -> Optic k p s t a b
+prism :: (Tensor k p, Coproduct k p) =>  (s `k` p a t) -> (b `k` t) -> Optic k p s t a b
 prism pre re = Optic $ \f -> f (elim inl inr . pre) (elim re id)
 
 {-# inline _Left #-}
@@ -382,7 +542,7 @@ _Left :: (Tensor k p, Coproduct k p) => Optic k p (p a c) (p b c) a b
 _Left = prism (elim inl (inr . inr)) inl
 
 {-# inline _Right #-}
-_Right :: (Tensor k p, Coproduct k p) => Optic k p (p c a) (p c b) a b
+_Right :: (Obj k b, Obj k (p c b), Tensor k p, Coproduct k p) => Optic k p (p c a) (p c b) a b
 _Right = prism (elim (inr . inl) inl) inr
 
 {-# inline _Just #-}
@@ -398,22 +558,27 @@ _Nothing = prism (maybe (inl ()) (inr . Just)) (\() -> Nothing)
 
 ---- Inspection Tests for fusion ----
 
-{-
+set_left1, set_left1' :: (Prelude.Functor b, Prelude.Functor c) => Functor.Product c (Functor.Sum a b) `Arr1` Functor.Sum c b
+set_left1 = set _Left
+set_left1' =
+  Arr1 $ \(Functor.Pair a e) ->
+  case e of
+    Functor.InL _ -> Functor.InL a
+    Functor.InR a -> Functor.InR a
+inspect ('set_left1 === 'set_left1')
+
+set_left, set_left' :: (a, Either a b) -> Either a b
+set_left = set _Left
+set_left' (a, e) =
+  case e of
+    Left _ -> Left a
+    Right a -> Right a
+inspect ('set_left === 'set_left')
+
 get_222, get_222' :: (a, (b, (c, d))) -> d
 get_222 = get (_2 `o` (_2 `o` _2))
 get_222' (_, (_, (_, a))) = a
-
 inspect ('get_222 === 'get_222')
-
-set_222, set_222' :: (e, (a, (b, (c, d)))) -> (a, (b, (c, e)))
-set_222 = set (_2 `o` (_2 `o` _2))
--- which is preferable?
-set_222' (d, (a, x)) = (a, case x of; (b, x') -> (b, case x' of; (c, _) -> (c, d)))
-set_222'' (d, (a, (b, (c, _)))) = (a, (b, (c, d)))
-
--- inspect ('set_222 === 'set_222')
-inspect ('set_222 === 'set_222'')
-
 
 preview_LJR, preview_LJR' :: Either (Maybe (Either a b)) c -> Either () b
 preview_LJR = preview (_Left `o `(_Just `o` _Right))
@@ -421,18 +586,15 @@ preview_LJR' x =
   case x of
     Left (Just (Right a)) -> Right a
     _ -> Left ()
-
-inspect ('preview_LJR === 'preview_LJR')
-
+inspect ('preview_LJR ==- 'preview_LJR')
 
 review_LJR = review (_Left `o `(_Just `o` _Right))
 review_LJR' = Left . Just . Right
-
 inspect ('review_LJR === 'review_LJR')
 
-
+set_LJR, set_LJR' :: (d, Either (Maybe (Either a b)) c) -> Either (Maybe (Either a d)) c
 set_LJR = set (_Left `o `(_Just `o` _Right))
-set_LJR' b x =
+set_LJR' (b, x) =
   case x of
     Left x' ->
       case x' of
@@ -442,15 +604,19 @@ set_LJR' b x =
             Right _ -> Left (Just (Right b))
             Left a -> Left (Just (Left a))
     Right a -> Right a
-set_LJR'' a (Left (Just (Right _))) = Left . Just . Right $ a
-set_LJR'' _ a = a
-
 inspect ('set_LJR === 'set_LJR')
 
----- w00t ----
-
-main = do
-  print . fst $ set_222' 'b' ('a', undefined)
-  print $ set_222' 'b' ('a', ('b', ('c', 'd')))
-  print . fst $ set_222'' 'b' ('a', undefined)
+{-
+set_222, set_222' :: (e, (a, (b, (c, d)))) -> (a, (b, (c, e)))
+set_222 = set (_2 `o` (_2 `o` _2))
+set_222' (x, a) =
+  case a of
+    (b, c) ->
+      case c of
+        (d, e) ->
+          case e of
+            (f, _) -> (b, (d, (f, x)))
+inspect ('set_222 === 'set_222')
 -}
+
+---- w00t ----
